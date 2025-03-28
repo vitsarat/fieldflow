0284c2acfb1219fbeb1fbaf1a44b4dcbe6198c0d
vitsarat <tongapplid@gmail.com>
2025-03-29 02:53:03 +0700

    Update profile.js

--- public/js/profile.js
+++ public/js/profile.js
@@ -3,7 +3,12 @@ import { getFirestore, collection, getDocs } from "https://www.gstatic.com/fireb
 import { auth, onAuthStateChanged } from "./auth.js";
 
 const firebaseConfig = {
-    // ใส่ Firebase Config ของคุณที่นี่
+apiKey: "AIzaSyDyOLP6v2mw5CRPMwVwynTU-qAAq8QMrlc",
+  authDomain: "fieldflow-b3ee5.firebaseapp.com",
+  projectId: "fieldflow-b3ee5",
+  storageBucket: "fieldflow-b3ee5.firebasestorage.app",
+  messagingSenderId: "384778621124",
+  appId: "1:384778621124:web:1a40999850200d49c63991"
 };
 
 const app = initializeApp(firebaseConfig);
