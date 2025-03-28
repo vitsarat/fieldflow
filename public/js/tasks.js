1aa2cf06b987580755d4335032565539321f9e4c
vitsarat <tongapplid@gmail.com>
2025-03-29 02:19:36 +0700

    Update tasks.js

--- public/js/tasks.js
+++ public/js/tasks.js
@@ -1,4 +1,4 @@
-// js/tasks.js
+// public/js/tasks.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
