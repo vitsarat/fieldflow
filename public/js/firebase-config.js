2dbebfb0e2127a7e895690bf1041eb8437e0d2b4
vitsarat <tongapplid@gmail.com>
2025-03-29 01:38:22 +0700

    Delete public/js/firebase-config.js

--- public/js/firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDyOLP6v2mw5CRPMwVwynTU-qAAq8QMrlc",
-    authDomain: "fieldflow-b3ee5.firebaseapp.com",
-    projectId: "fieldflow-b3ee5",
-    storageBucket: "fieldflow-b3ee5.firebasestorage.app",
-    messagingSenderId: "384778621124",
-    appId: "1:384778621124:web:1a40999850200d49c63991"
-};
