9149f1d62517e4ce0c8347d193965406bd991540
vitsarat <tongapplid@gmail.com>
2025-03-29 02:00:41 +0700

    Update admin.js

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,4 +1,4 @@
-// js/admin.js
+// public/js/admin.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore, collection, getDocs, addDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
