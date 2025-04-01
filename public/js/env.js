0d141593fc66c8b0ce5ae79a4c473b692f9ca3a5
vitsarat <tongapplid@gmail.com>
2025-04-01 17:24:27 +0700

    Update env.js

--- public/js/env.js
+++ public/js/env.js
@@ -1,8 +1,8 @@
 window.env = {
-    FIREBASE_API_KEY: "AIzaSyBIO5vI2BnvXOYu47T2RE4p2imSN3HCI9Y",
+    FIREBASE_API_KEY: "AIzaSyBI05V12BnvX0vU4712RE4P2iMSN3HCi9Y",
     FIREBASE_AUTH_DOMAIN: "fieldflow-efabd.firebaseapp.com",
     FIREBASE_PROJECT_ID: "fieldflow-efabd",
     FIREBASE_STORAGE_BUCKET: "fieldflow-efabd.firebasestorage.app",
     FIREBASE_MESSAGING_SENDER_ID: "147924792458",
-    FIREBASE_APP_ID: "NEW_APP_ID"
+    FIREBASE_APP_ID: "1:147924792458:web:187a1c72b49abaf1e973"
 };
