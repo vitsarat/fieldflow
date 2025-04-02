ec7c3b9baf04dd1ce3a7b8eecfa2f7f30673d19c
vitsarat <tongapplid@gmail.com>
2025-04-02 16:54:24 +0700

    Update auth.js

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,18 +1,22 @@
-// js/auth.js
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js';
-import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js';
-import { getFirestore, doc, getDoc, setDoc } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-// Initialize Firebase App
-const firebaseConfig = window.env;
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDyOLP6v2mw5CRPMwVwynTU-qAAq8QMrlc",
+  authDomain: "fieldflow-b3ee5.firebaseapp.com",
+  projectId: "fieldflow-b3ee5",
+  storageBucket: "fieldflow-b3ee5.firebasestorage.app",
+  messagingSenderId: "384778621124",
+  appId: "1:384778621124:web:ea912e4770f1811ec63991"
+};
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Auth and Firestore
 const auth = getAuth(app);
-const db = getFirestore(app);
 
-// Export for use in other files
-export { app, auth, db, signInWithEmailAndPassword, onAuthStateChanged, signOut };
+export { auth, signInWithEmailAndPassword };
 
 // Check if user is logged in, redirect to index.html if not
 onAuthStateChanged(auth, async (user) => {
